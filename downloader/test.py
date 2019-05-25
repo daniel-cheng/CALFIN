26fb3729913d74d1aa47eb586e75291a6c4baaae
Daniel Cheng <dcheng334@gmail.com>
2019-05-25 15:43:37 -0700

    ADD: Landsat downloader script.

---
+++ downloader/test.py
@@ -0,0 +1,11 @@
+import os,requests
+def download(url):
+    get_response = requests.get(url,stream=True)
+    file_name  = url.split("/")[-1]
+    with open(file_name, 'wb') as f:
+        for chunk in get_response.iter_content(chunk_size=1024):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+
+
+download("https://earthexplorer.usgs.gov/download/12267/LE70140092013272ASN00/STANDARD/EE")
