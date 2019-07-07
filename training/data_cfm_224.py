7e8b42485b02a4b1ade630bb637d9588452607a1
Daniel Cheng <dcheng334@gmail.com>
2019-07-06 20:18:33 -0700

    ADD: mobilenetv2 networks.

---
+++ training/data_cfm_224.py
@@ -0,0 +1,111 @@
+from __future__ import print_function
+
+import os, cv2, skimage, glob
+import numpy as np
+
+from skimage.transform import resize
+from skimage.io import imsave, imread
+from random import shuffle
+from aug_generators import aug_daniel, create_unagumented_data_from_image
+
+data_path = 'data/'
+temp_path = 'temp/'
+img_size = 224
+image_stride = img_size / 4
+image_stride_range = 9
+
+edge_filter = np.array((
+	[-1, -1, -1],
+	[-1, 8, -1],
+	[-1, -1, -1]), dtype="int")
+
+def extractPatches(im, window_shape=(img_size, img_size), stride=image_stride):
+	#In order to extract patches, determine the resolution of the image needed to extract regular patches
+	#Pad image if necessary
+#	print(im.shape)
+	nr, nc = im.shape
+
+	#If image is smaller than window size, pad to fit.
+	#else, pad to the least integer multiple of stride
+	#Window shape is assumed to multiple of stride.
+	#Find the smallest multiple of stride that is greater than image dimensions
+	leastRowStrideMultiple = (np.ceil(nr / stride) * stride).astype(np.uint16)
+	leastColStrideMultiple = (np.ceil(nc / stride) * stride).astype(np.uint16)
+	#If image is smaller than window, pad to window shape. Else, pad to least stride multiple.
+	nrPad = max(window_shape[0], leastRowStrideMultiple) - nr
+	ncPad = max(window_shape[1], leastColStrideMultiple) - nc
+	#Add Stride border around image, and nrPad/ncPad to image to make sure it is divisible by stride.
+	stridePadding = int(stride / 2)
+	paddingRow = (stridePadding, nrPad + stridePadding)
+	paddingCol = (stridePadding, ncPad + stridePadding)
+	padding = (paddingRow, paddingCol)
+	imPadded = np.pad(im, padding, 'constant')
+
+	patches = skimage.util.view_as_windows(imPadded, window_shape, stride)
+	nR, nC, H, W = patches.shape
+	nWindow = nR * nC
+	patches = np.reshape(patches, (nWindow, H, W))
+	return patches, nr, nc, nR, nC
+
+def load_validation_data(img_size):
+	imgs_validation = np.load('landsat_imgs_validation_boundaries_' + str(img_size) + '.npy').astype(np.float32)
+	imgs_mask_validation = np.load('landsat_imgs_mask_validation_boundaries_' + str(img_size) + '.npy').astype(np.float32)
+	return (imgs_validation, imgs_mask_validation)
+
+def create_validation_data_from_directory(img_size):
+	validation_data_path = 'data/validation'
+	temp_path = 'temp/validation'
+	images = glob.glob(validation_data_path + '/*[0-9].png')
+	shuffle(images)
+	total = len(images)
+	imgs = None
+	imgs_mask = None
+	i = 0
+	
+	print('-'*30)
+	print('Creating validation images...')
+	print('-'*30)
+	for image_path in images:
+		image_name = image_path.split(os.path.sep)[-1]
+		image_mask_name = image_name.split('.')[0] + '_mask.png'
+		img_uint16 = imread(os.path.join(validation_data_path, image_name), as_gray=True) #np.uint16 [0, 65535]
+		mask_uint16 = imread(os.path.join(validation_data_path, image_mask_name), as_gray=True) #np.uint16 [0, 65535]
+		img_f64 = resize(img_uint16, (img_size, img_size), preserve_range=True)  #np.float64 [0.0, 65535.0]
+		mask_f64 = resize(mask_uint16, (img_size, img_size), preserve_range=True) #np.float64 [0.0, 65535.0]
+		
+		#Convert greyscale to RGB greyscale, preserving as max range as possible in uint8 
+		img_max = img_f64.max()
+		mask_max = mask_f64.max()
+		if (img_max != 0.0):
+			img_uint8 = np.round(img_f64 / img_max * 255.0).astype(np.uint8) #np.uint8 [0, 255]
+		if (mask_max != 0.0):
+			mask_uint8 = np.floor(mask_f64 / mask_max * 255.0).astype(np.uint8) #np.uint8 [0, 255]
+		
+		#Calculate edge from mask and dilate.
+		kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+		mask_edge = cv2.Canny(mask_uint8.astype(np.uint8), 250, 255 * 2) #thresholds = Use diagonals to detect strong edges, then connect anything with at least a single edge
+		mask_edge = cv2.dilate(mask_edge.astype('float64'), kernel, iterations = 1)
+		mask_edge = np.where(mask_edge > 127, 1.0, 0.0).astype('float32') #np.float32 [0.0, 1.0]
+		
+		patches, maskPatches = create_unagumented_data_from_image(img_uint8, mask_edge)
+		
+		imsave(os.path.join(temp_path, image_name), np.round((patches[0,:,:,0] + 1) / 2 * 255).astype(np.uint8))
+		imsave(os.path.join(temp_path, image_mask_name), (255 * maskPatches[0,:,:,0]).astype(np.uint8))
+	
+		if (imgs is not None):
+			imgs = np.concatenate((imgs, patches))
+			imgs_mask = np.concatenate((imgs_mask, maskPatches))
+			if (imgs.shape[0] != imgs_mask.shape[0]):
+				raise ValueError()
+		else:
+			imgs = patches
+			imgs_mask = maskPatches
+
+		i += 1
+		print('Done: {0}/{1} images'.format(i, total))
+
+	np.save('landsat_imgs_validation_boundaries_' + str(img_size) + '.npy', imgs)
+	np.save('landsat_imgs_mask_validation_boundaries_' + str(img_size) + '.npy', imgs_mask)
+		
+if __name__ == '__main__':
+	create_validation_data_from_directory(img_size)
---
+++ training/train_cfm_320_deeplabv3_mobilenetv2.py
@@ -0,0 +1,90 @@
+from __future__ import print_function
+
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.preprocessing.image import ImageDataGenerator
+from keras.utils import plot_model
+from keras.models import Model, Input, load_model
+from keras.layers import Concatenate, Conv2D, MaxPooling2D, Conv2DTranspose, Dropout, UpSampling2D, BatchNormalization, RepeatVector, Reshape, Permute, Flatten
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
+from keras.regularizers import l1, l2, l1_l2
+from keras.activations import relu, sigmoid
+from keras.layers import Activation
+from keras import backend as K
+from tensorflow.python.client import device_lib
+from keras.applications import imagenet_utils
+from segmentation_models.losses import bce_jaccard_loss, jaccard_loss, binary_crossentropy
+from segmentation_models.metrics import iou_score
+
+import sys
+sys.path.insert(0, 'keras-deeplab-v3-plus')
+from model import Deeplabv3
+from clr_callback import CyclicLR
+from AdamAccumulate import AdamAccumulate
+
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+
+import cv2, glob
+from skimage.io import imsave, imread
+from skimage.transform import resize, rotate, rescale
+from random import shuffle
+
+from data_cfm_320 import load_validation_data
+from albumentations import *
+from aug_generators import aug_daniel, imgaug_generator
+
+img_size = 320
+data_path = 'data/'
+pred_path = 'preds/'
+temp_path = 'temp/'
+K.set_image_data_format('channels_last')  # TF dimension ordering in this code
+
+if __name__ == '__main__':
+	print('-'*30)
+	print('Loading validation data...')
+	print('-'*30)
+	validation_data = load_validation_data(img_size) 
+	
+	model_checkpoint = ModelCheckpoint('cfm_weights_mobilenetv2_' + str(img_size) + '_e{epoch:02d}_iou{val_iou_score:.4f}.h5', monitor='val_iou_score', save_best_only=False)
+	clr_triangular = CyclicLR(mode='triangular2', step_size=4000, base_lr=6e-4, max_lr=6e-5)
+	callbacks_list = [
+		EarlyStopping(patience=6, verbose=1, restore_best_weights=False),
+#		clr_triangular,
+		model_checkpoint
+	]
+	
+	print('-'*30)
+	print('Creating and compiling model...')
+	print('-'*30)
+	img_shape = (img_size, img_size, 1)
+#	flatten_shape = (img_size * img_size,)
+#	target_shape = (img_size, img_size, 3)
+	inputs = Input(shape=img_shape)
+#	r1 = Reshape(flatten_shape)(inputs)
+#	r2 = RepeatVector(3)(r1)
+#	r3 = Reshape(target_shape)(r2)
+	base_model = Deeplabv3(input_shape=img_shape, classes=1, alpha = 1.4, backbone='mobilenetv2', weights=None)
+	last_linear = base_model(inputs)
+	out = Activation('sigmoid')(last_linear)
+	
+	model = Model(inputs, out)
+	model.compile(optimizer=AdamAccumulate(lr=1e-4, accum_iters=16), loss=bce_jaccard_loss, metrics=['binary_crossentropy', iou_score, 'accuracy'])
+	model.summary()
+#	model.load_weights('cfm_weights_320_e07_iou0.0139.h5')
+	
+	print('-'*30)
+	print('Fitting model...')
+	print('-'*30)
+	train_generator = imgaug_generator(1, img_size)
+	history = model.fit_generator(train_generator,
+				steps_per_epoch=16000,
+				epochs=10,
+				validation_data=validation_data,
+				verbose=1,
+#				max_queue_size=64,
+#				use_multiprocessing=True,
+#				workers=2,
+				callbacks=callbacks_list)
+	print(history.history)
