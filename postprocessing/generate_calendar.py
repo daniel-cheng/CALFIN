5ab439986e426c45e13e6ef8a86bd7fa0963fa5c
Daniel Cheng <dcheng334@gmail.com>
2020-09-26 01:52:59 -0700

    CHG: More colorblind figures and updated calendar.

--- postprocessing/generate_calendar.py
+++ postprocessing/generate_calendar.py
@@ -1,58 +1,67 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  3 22:38:07 2019
+
+@author: Daniel
+"""
+
+import os
 from collections import defaultdict
+import pandas as pd
+os.environ['GDAL_DATA'] = r'D:\ProgramData\Anaconda3\envs\cfm\Library\share\gdal' #Ensure crs are exported correctly by gdal/osr/fiona
+import fiona
+from fiona.crs import from_epsg
 
-import sys, os
-sys.path.insert(1, '../training/keras-deeplab-v3-plus')
-sys.path.insert(2, '../training')
+def output_calendar_csv(dest_all_path, version, shp_type):
+    """Generate a 2d Table of the annual number of calving front per domain from consolidated shapefiles."""      
+    schema = {
+        'geometry': shp_type,
+        'properties': {
+            'GlacierID': 'int',
+            'Center_X': 'float',
+            'Center_Y': 'float',
+            'Latitude': 'float',
+            'Longitude': 'float',
+            'QualFlag': 'int',
+            'Satellite': 'str',
+            'Date': 'str',
+            'ImageID': 'str',
+            'GrnlndcN': 'str',
+            'OfficialN': 'str',
+            'AltName': 'str',
+            'RefName': 'str',
+            'Author': 'str'
+        },
+    }
+    if shp_type == 'LineString':
+        suffix = '_' + version + '.shp'
+    elif shp_type == 'Polygon':
+        suffix = '_closed_' + version + '.shp'
+    else:
+        raise ValueError('Unrecognized shp_type (should be "line" or "polygon"):', shp_type)    
+    output_all_shp_path = os.path.join(dest_all_path, 'termini_1972-2019_Greenland' + suffix)
+    calendar = defaultdict(lambda: defaultdict(int))
+    with fiona.open(output_all_shp_path, 
+            'r', 
+            driver='ESRI Shapefile', 
+            crs=fiona.crs.from_epsg(3413), 
+            schema=schema, 
+            encoding='utf-8') as output_all_shp_file:
+        for feature in output_all_shp_file:
+            date = feature['properties']['Date']
+            name = feature['properties']['RefName']
+            year = date.split('-')[0]
+            calendar[name][year] += 1
+    
+    calendar_path = os.path.join(dest_all_path, 'calendar.csv')
+    pd.DataFrame.from_dict(data=calendar, orient='columns').to_csv(calendar_path, header=True)
+    return calendar
 
-from plotting import plot_histogram, plot_scatter
+            
 
-def output_calendar_csv():
-	manual_qa_path = r'D:\Daniel\Documents\Github\CALFIN Repo\outputs\mask_extractor\quality_assurance'
-	auto_qa_path = r'D:\Daniel\Documents\Github\CALFIN Repo\outputs\production\quality_assurance'
-	auto_qa_bad_path = r'D:\Daniel\Documents\Github\CALFIN Repo\outputs\production\quality_assurance_bad'
-	
-	domain_validation_calendar = defaultdict(lambda: defaultdict(str))
-	original_names = []
-	scene_ids = []
-	
-	for domain in os.listdir(auto_qa_bad_path):
-		domain_path = os.path.join(auto_qa_bad_path, domain)
-		if os.path.isdir(domain_path):
-			for file_name in os.listdir(domain_path):
-				file_path = os.path.join(domain_path, file_name)
-				file_name_parts = file_name.split('_')
-				domain = file_name_parts[0]
-				date = file_name_parts[3]
-				domain_validation_calendar[domain][date] = "UNPICKED"
-	
-	for domain in os.listdir(auto_qa_path):
-		domain_path = os.path.join(auto_qa_path, domain)
-		if os.path.isdir(domain_path):
-			for file_name in os.listdir(domain_path):
-				file_path = os.path.join(domain_path, file_name)
-				file_name_parts = file_name.split('_')
-				domain = file_name_parts[0]
-				date = file_name_parts[3]
-				domain_validation_calendar[domain][date] = "AUTO-GOOD"
-	
-	for domain in os.listdir(manual_qa_path):
-		domain_path = os.path.join(manual_qa_path, domain)
-		if os.path.isdir(domain_path):
-			for file_name in os.listdir(domain_path):
-				file_path = os.path.join(domain_path, file_name)
-				file_name_parts = file_name.split('_')
-				domain = file_name_parts[0]
-				date = file_name_parts[3]
-				domain_validation_calendar[domain][date] = "MANUAL"
-			
-	"""Creates a csv file with the number of images per year per domain."""
-	calendar_name = 'calendar_expanded_updated.csv'
-	calendar_path = os.path.join(auto_qa_path, calendar_name)
-	pd.DataFrame.from_dict(data=domain_validation_calendar, orient='columns').to_csv(calendar_path, header=True)
-	return domain_validation_calendar
-
-if __name__ == '__main__':
-	domain_validation_calendar = output_calendar_csv()
+if __name__ == "__main__":    
+    version = "v1.0"
+    dest_all_path = r'../outputs/upload_production/v1.0/level-1_shapefiles-greenland-termini'
+    shp_types = 'LineString' #'Polygon', 'LineString'
+    
+    calendar = output_calendar_csv(dest_all_path, version, shp_types)
